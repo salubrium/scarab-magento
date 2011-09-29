d0fa213eacdb04ca2f776e9c2dbe6c99bb888e4e
prodigital45 <prodigital45@gmail.com>
2011-09-29 11:25:52 +0000

    scarab sales export file

---
+++ scarabsales.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+Manual catalog export
+ */
+
+require 'app/Mage.php';
+
+if (!Mage::isInstalled()) {
+    echo "Application is not installed yet. Please complete installation first.";
+    die;
+}
+
+Mage::app();
+
+try {
+	Mage::getModel('scarabresearch/salescron')->salesbackup();
+	echo "Export successful. Fresh Sales export is located at www.yourwebshopdomain.com/export/scarabsales.csv. You can now close this window.";
+} catch (Exception $e) {
+    Mage::printException($e);
+}
