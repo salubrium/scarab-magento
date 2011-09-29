8cd9cc506ef6813afd2d468d17da554813f5decc
prodigital45 <prodigital45@gmail.com>
2011-09-29 11:16:05 +0000

    custom date picker class

--- lib/Varien/Data/Form/Element/Pddateselection.php
+++ lib/Varien/Data/Form/Element/Pddateselection.php
@@ -1,1 +1,13 @@
+<?php
+class Varien_Data_Form_Element_PdDateSelection extends Varien_Data_Form_Element_Date
+{
+    public function getElementHtml()
+    {
+        // define image url
+        $this->setImage('/skin/adminhtml/default/default/images/grid-cal.gif');
+        // define date format
+        $this->setFormat('yyyy-MM-dd');
 
+        return parent::getElementHtml();
+    }
+}
