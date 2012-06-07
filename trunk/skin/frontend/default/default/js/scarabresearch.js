22070818359dbc6357bb02cf219453f2d703a379
prodigital45@gmail.com <prodigital45@gmail.com@c6acd95e-8b17-a979-6ca5-79f7d35103f5>
2012-06-07 10:46:38 +0000

    unused file in future versions

--- trunk/skin/frontend/default/default/js/scarabresearch.js
+++ trunk/skin/frontend/default/default/js/scarabresearch.js
@@ -1,4 +1,1 @@
-document.write(unescape("%3Cscript id='scarab-js-api' src='" + 
-   document.location.protocol + 
-   "//recommender.scarabresearch.com/scarab-v2.js'" +
-   "type='text/javascript'%3E%3C/script%3E"));
+
